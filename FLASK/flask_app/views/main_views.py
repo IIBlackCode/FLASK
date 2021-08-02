4cba286dd1b347086b54b5c66074f21d9b634d3c
김민서 <blackcode-@naver.com>
2021-08-02 09:11:30 +0900

    기본 페이지 구성 완료

--- FLASK/flask_app/views/main_views.py
+++ FLASK/flask_app/views/main_views.py
@@ -7,11 +7,42 @@ def hello_pybo():
     return 'Hello, Pybo!'
 
 @bp.route('/')
-def index():
+def foody_blog_index():
     return render_template('foody_blog/index.html')
+@bp.route('/404')
+def foody_blog_404():
+    return render_template('foody_blog/404.html')
+@bp.route('/about')
+def foody_blog_about():
+    return render_template('foody_blog/about.html')
+@bp.route('/categories-grid')
+def foody_blog_categories_grid():
+    return render_template('foody_blog/categories-grid.html')
+@bp.route('/categories-list')
+def foody_blog_categories_list():
+    return render_template('foody_blog/categories-list.html')
+@bp.route('/contact')
+def foody_blog_contact():
+    return render_template('foody_blog/contact.html')
+@bp.route('/main')
+def foody_blog_main():
+    return render_template('foody_blog/main.html')
+@bp.route('/signin')
+def foody_blog_signin():
+    return render_template('foody_blog/signin.html')
+@bp.route('/single-post')
+def foody_blog_single_post():
+    return render_template('foody_blog/single-post.html')
+@bp.route('/typography')
+def foody_blog_typography():
+    return render_template('foody_blog/typography.html')
 
+
+
+
+
+    
 #------------------------------ [Theme] ------------------------------#
-
 @bp.route('/theme/index')
 def theme_index():
     return render_template('theme/index.html')
